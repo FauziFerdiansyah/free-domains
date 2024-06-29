dd7649be83f493f5d52b499710a522d05b758ea5
Fauzi Ferdiansyah <fetruzie2@gmail.com>
2024-06-29 21:09:24 +0700

    feat: Domains definition using JS

---
+++ domains/bitha.is-an.app.js
@@ -0,0 +1,12 @@
+addSubDomain({
+  description: 'Personal site for my little shop',
+  domain: 'is-an.app',
+  subdomain: 'bitha',
+  owner: {
+    repo: 'https://github.com/FauziFerdiansyah/bitha',
+    email: 'fetruzie2@gmail.com',
+  },
+  record: {
+    CNAME: 'fauziferdiansyah.github.io',
+  }
+})
